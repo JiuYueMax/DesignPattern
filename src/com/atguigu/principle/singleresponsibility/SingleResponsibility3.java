15116405755fb35b8a3893319358d598d9cf2278
李九月 <1641986437@qq.com>
2023-06-18 18:48:20 +0800

    1111

--- src/com/atguigu/principle/singleresponsibility/SingleResponsibility3.java
+++ src/com/atguigu/principle/singleresponsibility/SingleResponsibility3.java
@@ -17,12 +17,7 @@ public class SingleResponsibility3 {
 //1. 这种修改方法没有对原来的类做大的修改，只是增加方法
 //2. 这里虽然没有在类这个级别上遵守单一职责原则，但是在方法级别上，仍然是遵守单一职责
 class Vehicle2 {
-	public void run(String vehicle) {
-		//处理
-		
-		System.out.println(vehicle + " 在公路上运行....");
-		
-	}
+	public void run(String vehicle) {System.out.println(vehicle + " 在公路上运行....");}
 	
 	public void runAir(String vehicle) {
 		System.out.println(vehicle + " 在天空上运行....");
